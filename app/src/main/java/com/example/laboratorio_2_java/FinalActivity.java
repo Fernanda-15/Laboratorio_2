ccbfe67209c2b61af917b78cc9923d61acf6aa61
Fernanda-15 <fernandaqp15@gmail.com>
2022-05-17 21:57:05 -0600

    Add Final Activity

--- app/src/main/java/com/example/laboratorio_2_java/FinalActivity.java
+++ app/src/main/java/com/example/laboratorio_2_java/FinalActivity.java
@@ -1,24 +1,14 @@
 package com.example.laboratorio_2_java;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
+public class FinalActivity extends AppCompatActivity {
 
-public class FinalActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message_activty);
-        Intent intent = getIntent(); //singleton
-        String message = intent.getStringExtra("Message");
-        TextView textView = findViewById(R.id.textView2);
-        textView.setText(message);
-
-        //comments for testing.
-
+        setContentView(R.layout.activity_final);
     }
 }
